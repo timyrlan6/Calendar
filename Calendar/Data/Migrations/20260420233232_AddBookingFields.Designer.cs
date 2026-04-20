7e27f1c63fd417e734cd445b9ecd15d6bbb7ee48
DESKTOP-UC2OCF1\Admin <timyrlan6@gmail.com>
2026-04-21 02:39:27 +0300

    feat: add booking migrations, create RULES.md with DB connection rules

--- Calendar/Data/Migrations/20260420233232_AddBookingFields.Designer.cs
+++ Calendar/Data/Migrations/20260420233232_AddBookingFields.Designer.cs
@@ -3,17 +3,20 @@ using System;
 using Calendar.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 #nullable disable
 
-namespace Calendar.Migrations
+namespace Calendar.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260420233232_AddBookingFields")]
+    partial class AddBookingFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
